39df67ca342a26f8abbb86f629c0d2a1e7c9d2b7
Omar <omaar_5@hotmail.com>
2021-02-20 23:05:55 +0200

    setup const routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import * as ROUTES from "./constants/routes";
 import HomeScreen from './Screens/HomeScreen';
+import SigninScreen from "./Screens/SigninScreen";
+import SignupScreen from "./Screens/SignupScreen";
+import BrowseScreen from "./Screens/BrowseScreen";
 
 function App() {
   return (
-    <div>
-      <HomeScreen />
-    </div>
+    <Router>
+      <Route exact path={ROUTES.HOME}>
+        <HomeScreen />
+      </Route>
+      <Route exact path={ROUTES.SIGNIN}>
+        <SigninScreen />
+      </Route>
+      <Route exact path={ROUTES.SIGNUP}>
+        <SignupScreen />
+      </Route>
+      <Route exact path={ROUTES.BROWSE}>
+        <BrowseScreen />
+      </Route>
+    </Router>
   );
 }
 
