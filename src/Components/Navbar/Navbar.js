1a12b2d53735976da7cd9c83a9dd18ab16b2ceaa
Omar <omaar_5@hotmail.com>
2021-02-23 20:24:21 +0200

    complete: BrowseContainer's Navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -13,10 +13,23 @@ import {
     UserProfile,
     Icon,
     DropDown,
+    BurgerContainer,
+    BurgerIcon,
+    BurgerIcons,
+    ResponsiveContainer,
+    ResponsiveLink,
+    LinkHold,
+    DisplayName,
+    UsersContainer,
+    DropLinks,
+    DropLink,
+    UserContainer,
+    SearchInput
 } from "./styles/styledNavbar";
 
 const Navbar = () => {
     const [show, setShow] = useState(false);
+    const [responsive, setResponsive] = useState(false);
     const user = auth.currentUser || {};
 
     const transitionNavbar = () => {
@@ -30,9 +43,9 @@ const Navbar = () => {
 
     return (
         <Container className={`${show && 'blackNav'}`}>
-
             <SubContainer>
                 <Netflix src={logo} alt="Netflix" />
+
                 <LinksContainer>
                     <Link>Home</Link>
                     <Link>TV Shows</Link>
@@ -40,18 +53,64 @@ const Navbar = () => {
                     <Link>New & Popular</Link>
                     <Link>My List</Link>
                 </LinksContainer>
+
+                <ResponsiveContainer>
+                    <BurgerIcons onClick={() => setResponsive(state => !state)}>
+                        <BurgerIcon src="/images/icons/burger.png" alt="Responsive" />
+                        <ChevronDown src="/images/icons/chevron-right.png" alt="Chevron Down" />
+                    </BurgerIcons>
+                    
+                    {responsive && (
+                        <BurgerContainer>
+                            <LinkHold>
+                                <ResponsiveLink>Home</ResponsiveLink>
+                            </LinkHold>
+                            <LinkHold>
+                                <ResponsiveLink>TV Shows</ResponsiveLink>
+                            </LinkHold>
+                            <LinkHold>
+                                <ResponsiveLink>Movies</ResponsiveLink>
+                            </LinkHold>
+                            <LinkHold>
+                                <ResponsiveLink>New & Popular</ResponsiveLink>
+                            </LinkHold>
+                            <LinkHold>
+                                <ResponsiveLink>My List</ResponsiveLink>
+                            </LinkHold>
+                        </BurgerContainer>
+                    )}
+                </ResponsiveContainer>
             </SubContainer>
 
             <SubContainer className="invertSubContainer">
                 <UserProfileContainer>
                     <UserProfile src={`/images/users/${user.photoURL}.png`} alt="Profile Picture" />
                     <ChevronDown src="/images/icons/chevron-right.png" alt="Chevron Down" />
+                    <DropDown>
+                        <DropLinks className="above">
+                            <UsersContainer>
+                                <UserContainer>
+                                    {/* manyUsers ? UserContainer.map */}
+                                    <UserProfile src={`/images/users/${user.photoURL}.png`} alt="Profile Picture" />
+                                    <DisplayName>{user.displayName}</DisplayName>
+                                </UserContainer>
+                            </UsersContainer>
+                            <DropLink>Manage Profiles</DropLink>
+                        </DropLinks>
+                        <DropLinks>
+                            <DropLink>Account</DropLink>
+                            <DropLink>Help Center</DropLink>
+                            <DropLink>Signout of Netflix</DropLink>
+                        </DropLinks>
+                    </DropDown>
                 </UserProfileContainer>
+                
                 <GroupContainer>
                     <Icon src="/images/icons/notifications.png" alt="Notfications" />
                     <Icon src="/images/icons/redeem.png" alt="Gift" />
                     <Link>KIDS</Link>
                     <Icon src="/images/icons/search.png" alt="Search" />
+                    <SearchInput type="text" placeholder="Titles,people,genres" />
                 </GroupContainer>
             </SubContainer>
         </Container>
--- src/Components/Navbar/styles/styledNavbar.js
+++ src/Components/Navbar/styles/styledNavbar.js
@@ -31,8 +31,44 @@ export const Netflix = styled.img`
 `;
 
 export const LinksContainer = styled.div`
-    /* border: 2px solid red; */
     margin-left: 20px;
+
+    @media (max-width: 1300px) {
+        display: none;
+    }
+`;
+
+export const ResponsiveContainer = styled.div`
+    position: relative;
+`;
+
+export const BurgerIcons = styled.div`
+    cursor: pointer;
+    display: none;
+    margin-left: 30px;
+    align-items: center;
+
+    @media (max-width: 1300px) {
+        display: flex;
+    }
+`;
+
+export const BurgerIcon = styled.img`
+    filter: invert(1);
+    width: 40px;
+    margin-right: 10px;
+`;
+
+export const BurgerContainer  = styled.div`
+    border-top: 1px solid white;
+    display: flex;
+    flex-direction: column;
+    position: absolute;
+    left: 0;
+    right: 0;
+    width: 200px;
+    background: none;
+    margin: 10px auto 0;
 `;
 
 export const Link = styled.button`
@@ -46,11 +82,94 @@ export const Link = styled.button`
     font-weight: 450;
 `;
 
+export const LinkHold = styled.div`
+    background: rgba(0, 0, 0, 0.7);
+    text-align: center;
+    transition: background 0.5s ease;
+    cursor: pointer;
+
+    &:hover {
+        background-color: rgba(0, 0, 0, 0.6);
+    }
+`;
+
+export const ResponsiveLink = styled.button`
+    background: none;
+    border: 0;
+    outline: 0;
+    cursor: pointer;
+    color: #808080;
+    font-weight: 600;
+    font-size: 1rem;
+    margin: 10px 0;
+`;
+
+export const DropDown = styled.div`
+    box-shadow: 0 0 2px 2px #000;
+    background-color: rgba(0, 0, 0, 0.8);
+    width: 150px;
+    height: 300px;
+    position: absolute;
+    top: 45px;
+    right: 30px;
+    /* display: flex; */
+    flex-direction: column;
+    display: none;
+`;
 
 export const UserProfileContainer = styled.div`
     cursor: pointer;
     display: flex;
     align-items: center;
+    position: relative;
+
+    &:hover > ${DropDown} {
+        display: flex;
+    }
+`;
+
+export const DisplayName = styled.h3`
+    color: #fff;
+    font-size: 14px;
+
+    &:hover {
+        text-decoration: underline;
+    }
+`;
+
+export const UsersContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
+export const UserContainer = styled.div`
+    max-height: 50px;
+    display: flex;
+    padding: 5px;
+    align-items: center;
+`;
+
+export const DropLinks = styled.div`
+    /* border: 2px solid red; */
+
+    &.above {
+        border-bottom: 2px solid white;
+    }
+`;
+
+export const DropLink = styled.button`
+    background: none;
+    color: #fff;
+    margin: 5px;
+    border: none;
+    outline: none;
+    cursor: pointer;
+    padding: 5px;
+    font-weight: 700;
+
+    &:hover {
+        text-decoration: underline;
+    }
 `;
 
 export const UserProfile = styled.img`
@@ -79,4 +198,11 @@ export const Icon = styled.img`
     cursor: pointer;
 `;
 
-export const DropDown = styled.div``;
+export const SearchInput = styled.input`
+    border: 2px solid white;
+    outline: none;
+    color: #808080;
+    padding: 8px;
+    background: rgba(0, 0, 0, 0.7);
+    /*background: rgba(64, 64, 64, 0.5); */
+`;
