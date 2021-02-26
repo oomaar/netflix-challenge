9d9016ef2b4680947b34d9e0af278bda0ef3853d
Omar <omaar_5@hotmail.com>
2021-02-27 00:43:54 +0200

    complete: cards

--- src/Components/BrowseContainer/BrowseContainer.js
+++ src/Components/BrowseContainer/BrowseContainer.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { Loading, Profiles, Navbar, Card } from '..';
+import { Loading, Profiles, Navbar, Card, Test} from '..';
 import { auth } from '../../lib/firebase';
 import { Background } from '../Header/styles/styledHeader';
 import {
@@ -65,7 +65,7 @@ const BrowseContainer = ({ slides }) => {
 
             </Background>
 
-            <Card.Group>
+            {/* <Card.Group>
                 {slideRows.map(slideItem => (
                     <Card key={`${category}-${slideItem.title.toLowerCase()}`}>
                         <Card.Title>{slideItem.title}</Card.Title>
@@ -81,16 +81,44 @@ const BrowseContainer = ({ slides }) => {
                                 </Card.Item>
                             ))}
                         </Card.Entities>
-                        {/* <Card.Feature category={category}>
+                        <Card.Feature category={category}>
                             <Player>
                                 <Player.Button />
                                 <Player.Video src="/videos/Paranormal.mp4" />
                             </Player>
-                        </Card.Feature> */}
+                        </Card.Feature> 
 
                     </Card>
                 ))}
+            </Card.Group> */}
+            <Card.Group>
+                {slideRows.map((slideItem) => (
+                    <Card key={`${category}-${slideItem.title.toLowerCase()}`}>
+                        <Card.Title>{slideItem.title}</Card.Title>
+                        <Card.Entities>
+                            {slideItem.data.map(item => (
+                                <Card.Item key={item.docId} item={item}>
+                                    <Card.Image src={`/images/${category}/${item.genre}/${item.slug}/small.jpg`} />
+                                    <Card.Meta>
+                                        <Card.SubTitle>{item.title}</Card.SubTitle>
+                                        <Card.Text>{item.description}</Card.Text>
+                                    </Card.Meta>
+                                </Card.Item>
+                            ))}
+                        </Card.Entities>
+                        <Card.Feature category={category}>
+                            {/* <Player>
+                                <Player.Button />
+                                <Player.Video src="/videos/bunny.mp4" />
+                            </Player> */}
+                            <p>Hello !</p>
+                        </Card.Feature>
+                    </Card>
+                ))}
+
             </Card.Group>
+
+            <h1>Footer</h1>
         </Container>
     ) : (
             <Profiles user={user} setProfile={setProfile} />
--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,21 +1,22 @@
-import { createContext, useContext, useState } from 'react';
+import React, { useState, useContext, createContext } from 'react';
+
 import {
     Container,
     Group,
     Title,
     SubTitle,
     Text,
-    Meta,
-    Item,
-    Image,
-    Entities,
     Feature,
     FeatureTitle,
     FeatureText,
     FeatureClose,
     Maturity,
     Content,
-} from "./styles/styledCard";
+    Meta,
+    Entities,
+    Item,
+    Image,
+} from './styles/styledCard';
 
 export const FeatureContext = createContext();
 
@@ -28,26 +29,30 @@ export default function Card({ children, ...restProps }) {
             <Container {...restProps}>{children}</Container>
         </FeatureContext.Provider>
     );
-};
+}
 
 Card.Group = function CardGroup({ children, ...restProps }) {
-    return <Group {...restProps}>{children}</Group>
+    return <Group {...restProps}>{children}</Group>;
 };
 
 Card.Title = function CardTitle({ children, ...restProps }) {
-    return <Title {...restProps}>{children}</Title>
+    return <Title {...restProps}>{children}</Title>;
 };
 
 Card.SubTitle = function CardSubTitle({ children, ...restProps }) {
-    return <SubTitle {...restProps}>{children}</SubTitle>
+    return <SubTitle {...restProps}>{children}</SubTitle>;
 };
 
 Card.Text = function CardText({ children, ...restProps }) {
-    return <Text {...restProps}>{children}</Text>
+    return <Text {...restProps}>{children}</Text>;
+};
+
+Card.Entities = function CardEntities({ children, ...restProps }) {
+    return <Entities {...restProps}>{children}</Entities>;
 };
 
 Card.Meta = function CardMeta({ children, ...restProps }) {
-    return <Meta {...restProps}>{children}</Meta>
+    return <Meta {...restProps}>{children}</Meta>;
 };
 
 Card.Item = function CardItem({ item, children, ...restProps }) {
@@ -55,11 +60,11 @@ Card.Item = function CardItem({ item, children, ...restProps }) {
 
     return (
         <Item
-            {...restProps}
             onClick={() => {
                 setItemFeature(item);
                 setShowFeature(true);
             }}
+            {...restProps}
         >
             {children}
         </Item>
@@ -67,9 +72,30 @@ Card.Item = function CardItem({ item, children, ...restProps }) {
 };
 
 Card.Image = function CardImage({ ...restProps }) {
-    return <Image {...restProps} />
+    return <Image {...restProps} />;
 };
 
-Card.Entities = function CardEntities({ children, ...restProps }) {
-    return <Entities {...restProps}>{children}</Entities>
-}
+Card.Feature = function CardFeature({ children, category, ...restProps }) {
+    const { showFeature, itemFeature, setShowFeature } = useContext(FeatureContext);
+
+    return showFeature ? (
+        <Feature {...restProps} src={`/images/${category}/${itemFeature.genre}/${itemFeature.slug}/large.jpg`}>
+            <Content>
+                <FeatureTitle>{itemFeature.title}</FeatureTitle>
+                <FeatureText>{itemFeature.description}</FeatureText>
+                <FeatureClose onClick={() => setShowFeature(false)}>
+                    <img src="/images/icons/close.png" alt="Close" />
+                </FeatureClose>
+
+                <Group margin="30px 0" flexDirection="row" alignItems="center">
+                    <Maturity rating={itemFeature.maturity}>{itemFeature.maturity < 12 ? 'PG' : itemFeature.maturity}</Maturity>
+                    <FeatureText fontWeight="bold">
+                        {itemFeature.genre.charAt(0).toUpperCase() + itemFeature.genre.slice(1)}
+                    </FeatureText>
+                </Group>
+
+                {children}
+            </Content>
+        </Feature>
+    ) : null;
+};
